f1ccb8a8c6fbf9c9232fc92c2cf95e59a27d970b
s3pt3mb3r <s3pt3mb3r@protonmail.com>
2021-07-17 16:44:21 +0800

    fixed a huge bug where it didn't scan entire dlls.
    now it's able to scan every dlls properly and find desired function definitely.

--- Notepad-IATHook-demo/main.cpp
+++ Notepad-IATHook-demo/main.cpp
@@ -5,81 +5,36 @@
 #include <vector>
 
 const char *processName = "notepad.exe";
-const char *targetDll = nullptr;
-const char *targetFunc = "NtCreateFile";
+const char *targetDll = "ntdll.dll";
+const char *targetFunc = "CreateFileW";
 
-std::vector<HMODULE> scanedDlls{};
+auto FindDlls(const char *moduleName) -> void;
 
-auto IsScaned(HMODULE &candidate) -> bool
+std::vector<std::string> scanedDllName{};
+std::vector<HMODULE> moduleHandles{};
+
+auto IsScaned(std::string &candidate)
 {
-    for (auto &scaned : scanedDlls)
+    for (auto &scaned : scanedDllName)
     {
         if (candidate == scaned)
         {
             return true;
         }
     }
-    scanedDlls.push_back(candidate);
+    scanedDllName.push_back(candidate);
     return false;
-}
-
-auto FindDlls(const char *moduleName) -> void
-{
-    static bool found = false;
-    if (found == true)
-    {
-        return;
-    }
-    HMODULE hModule;
-    BOOL err = GetModuleHandleEx(0, moduleName, &hModule);
-    //std::cout << "[+] module handle: 0x" << std::hex << hModule << std::endl;
-    if (!err || IsScaned(hModule))
-    {
-        return;
-    }
-
-    //if (moduleName != 0)
-    //{
-    //    std::cout << moduleName << std::endl;
-    //}
-
-    PIMAGE_DOS_HEADER pDosHeader = reinterpret_cast<PIMAGE_DOS_HEADER>(hModule);
-    DWORD dosBase = reinterpret_cast<DWORD>(pDosHeader);
-    PIMAGE_NT_HEADERS pNtHeaders = reinterpret_cast<PIMAGE_NT_HEADERS>(dosBase + pDosHeader->e_lfanew);
-    PIMAGE_OPTIONAL_HEADER pOptionalHeader = &pNtHeaders->OptionalHeader;
-    PIMAGE_IMPORT_DESCRIPTOR pImportDescriptor = reinterpret_cast<PIMAGE_IMPORT_DESCRIPTOR>(dosBase + pOptionalHeader->DataDirectory[IMAGE_DIRECTORY_ENTRY_IMPORT].VirtualAddress);
-
-    for (; pImportDescriptor->Name != NULL; ++pImportDescriptor)
-    {
-        char *dllName = reinterpret_cast<char *>(dosBase + pImportDescriptor->Name);
-        if (std::string(dllName) != "")
-        {
-            DWORD *ILTBase = reinterpret_cast<DWORD *>(dosBase + pImportDescriptor->OriginalFirstThunk); //Import lookup table pointer is somewhere on the memory and OriginalFirstThunk tells you the relative location.
-
-            for (int i = 0; *(ILTBase + i) != NULL; ++i)
-            {
-                PIMAGE_IMPORT_BY_NAME dllInfo = reinterpret_cast<PIMAGE_IMPORT_BY_NAME>(dosBase + *(ILTBase + i));
-                if (!strcmp(dllInfo->Name, targetFunc))
-                {
-                    std::cout << dllInfo->Name << std::endl;
-                    found = true;
-                    return;
-                }
-            }
-            FindDlls(dllName);
-        }
-    }
 }
 
 auto ReleaseModuleHandles() -> void
 {
-    for (auto &hModule : scanedDlls)
+    for (auto &hModule : moduleHandles)
     {
         CloseHandle(hModule);
     }
 }
 
-auto main() -> int
+int main()
 {
     PROCESSENTRY32 PE32{0};
     PE32.dwSize = sizeof(PE32);
@@ -113,7 +68,53 @@ auto main() -> int
         system("PAUSE");
         return 0;
     }
-    FindDlls(0);
 
+    FindDlls(0);
     ReleaseModuleHandles();
+    CloseHandle(hProc);
+}
+
+auto FindDlls(const char *moduleName) -> void
+{
+    static bool found = false;
+
+    HMODULE hModule;
+    BOOL err = GetModuleHandleEx(0, moduleName, &hModule);
+    //std::cout << "[+] module handle: 0x" << std::hex << hModule << std::endl;
+    if (!err)
+    {
+        moduleHandles.push_back(hModule);
+    }
+
+    PIMAGE_DOS_HEADER pDosHeader = reinterpret_cast<PIMAGE_DOS_HEADER>(hModule);
+    DWORD dosBase = reinterpret_cast<DWORD>(pDosHeader);
+    PIMAGE_NT_HEADERS pNtHeaders = reinterpret_cast<PIMAGE_NT_HEADERS>(dosBase + pDosHeader->e_lfanew);
+    PIMAGE_OPTIONAL_HEADER pOptionalHeader = &pNtHeaders->OptionalHeader;
+    PIMAGE_IMPORT_DESCRIPTOR pImportDescriptor = reinterpret_cast<PIMAGE_IMPORT_DESCRIPTOR>(dosBase + pOptionalHeader->DataDirectory[IMAGE_DIRECTORY_ENTRY_IMPORT].VirtualAddress);
+
+    for (; pImportDescriptor->Name != NULL; ++pImportDescriptor)
+    {
+        char *dllName = reinterpret_cast<char *>(dosBase + pImportDescriptor->Name);
+
+        if (found == true)
+        {
+            return;
+        }
+        if (std::string(dllName) != "" && !IsScaned(std::string(dllName)))
+        {
+            //std::cout << dllName << std::endl;
+            DWORD *ILTBase = reinterpret_cast<DWORD *>(dosBase + pImportDescriptor->OriginalFirstThunk); //Import lookup table pointer is somewhere on the memory and OriginalFirstThunk tells you the relative location.
+
+            for (int i = 0; *(ILTBase + i) != NULL; ++i)
+            {
+                PIMAGE_IMPORT_BY_NAME dllInfo = reinterpret_cast<PIMAGE_IMPORT_BY_NAME>(dosBase + *(ILTBase + i));
+                if (!strcmp(dllInfo->Name, targetFunc))
+                {
+                    found = true;
+                    //std::cout << dllInfo->Name << std::endl;
+                }
+            }
+            FindDlls(dllName);
+        }
+    }
 }
